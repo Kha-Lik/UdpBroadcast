550cfd91877d37e9705c022b5c93612500e83878
MykolaV <kolya.vasevich@gmail.com>
2021-09-18 20:30:27 +0300

    Initial commit

---
+++ UdpBroadcastClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdpBroadcastClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
