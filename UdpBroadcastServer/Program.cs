557a3a43b3d80ee0f72a60ed70d98d7f5fdad9f5
MykolaV <kolya.vasevich@gmail.com>
2021-09-18 21:24:04 +0300

    finished server

--- UdpBroadcastServer/Program.cs
+++ UdpBroadcastServer/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace UdpBroadcastServer
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        private static Server _server;
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            _server = new Server(8844, "127.0.0.1");
+            _server.Init();
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("==================\n" +
+                                  "\t1 - Send message\n" +
+                                  "\t2 - Exit\n");
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        await SendMessage();
+                        break;
+                    case "2":
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Error! Unexpected input!");
+                        break;
+                }
+            }
+        }
+
+        private static async Task SendMessage()
+        {
+            Console.WriteLine("Enter message:\n");
+            var input = Console.ReadLine();
+
+            await _server.SendMessageAsync(input);
         }
     }
 }
---
+++ UdpBroadcastServer/Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpBroadcastServer
+{
+    public class Server
+    {
+        private const string Path = @"./clients.txt";
+        private readonly int _port;
+        private readonly string _ip;
+        private List<IPEndPoint> _clients;
+        private Socket _socket;
+        
+        
+        public Server(int port, string ip)
+        {
+            _port = port;
+            _ip = ip;
+            
+            ParseClients();
+        }
+
+        public void Init()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            var endpoint = new IPEndPoint(IPAddress.Parse(_ip), _port);
+            _socket.Bind(endpoint);
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            foreach (var ipEndPoint in _clients)
+            {
+                var data = Encoding.Unicode.GetBytes(message);
+                await _socket.SendToAsync(data, SocketFlags.None, ipEndPoint);
+                Console.WriteLine($"Sent message to client {ipEndPoint.Address}:{ipEndPoint.Port}");
+            }
+        }
+
+        private void ParseClients()
+        {
+            if (!File.Exists(Path)) throw new FileNotFoundException(Path);
+
+            var lines = File.ReadAllLines(Path);
+
+            List<Tuple<string, int>> adresses = lines
+                .Select(l => l.Split(':'))
+                .Select(pair => new Tuple<string, int>(pair[0], int.Parse(pair[1]))).ToList();
+
+            _clients = adresses.Select(t => new IPEndPoint(IPAddress.Parse(t.Item1), t.Item2)).ToList();
+        }
+    }
+}
